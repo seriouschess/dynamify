ed2492fe8292bda5ed384081d4521c7ab39d987e
seriouschess <rehayemb@gmail.com>
2020-02-22 13:45:44 -0600

    Added authentication generator method

---
+++ Classes/Auth/Token.cs
@@ -0,0 +1,12 @@
+namespace dynamify.Classes.Auth
+{
+    public class Token
+    {
+        public string token {get;set;}
+
+        public Token( string token_string ){
+            token = token_string;
+        }
+
+    } 
+}
