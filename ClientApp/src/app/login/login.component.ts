103a6a19e3d0f218f620e9c5849ea355f1265b23
seriouschess <rehayemb@gmail.com>
2020-03-03 15:12:43 -0600

    Frontend validations added for login/registration

--- ClientApp/src/app/login/login.component.ts
+++ ClientApp/src/app/login/login.component.ts
@@ -8,6 +8,7 @@ import { HttpService } from '../http.service';
 })
 
 export class LoginComponent implements OnInit {
+  
 
   constructor(private _httpService:HttpService) { }
   @Output() logEvent = new EventEmitter<Admin>();
@@ -15,7 +16,19 @@ export class LoginComponent implements OnInit {
   newAdminObject: Admin;
   entered_email: string;
   entered_password: string;
+  
+
+  //validation fields
   error_display: string;
+  registration_password_error_flag: boolean;
+  password_confirm: string;
+  password_confirm_error_flag:boolean;
+  first_name_required_error:boolean;
+  last_name_required_error:boolean;
+  email_validation_error_flag: boolean;
+  login_password_validation_error_flag: boolean;
+  login_email_validation_error_flag: boolean;
+  
 
    ngOnInit() {
     this.newAdminObject = {
@@ -39,52 +52,140 @@ export class LoginComponent implements OnInit {
     this.entered_email = "";
     this.entered_password ="";
     this.error_display = "";
+    this.registration_password_error_flag = false;
+    this.password_confirm = "";
+    this.password_confirm_error_flag = false;
+    this.email_validation_error_flag = false;
+    this.login_email_validation_error_flag = false;
+    this.login_password_validation_error_flag = false;
+    this.first_name_required_error = false;
+    this.last_name_required_error = false;
     this.logEvent.emit(this.logged_admin); //zero is the default non sql ID value
   }
 
+  validateEmail(email:string):boolean {
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+  
+  validateRegistration():boolean{ //returns true of registration information is valid
+    let error_count = 0;
+
+    if(this.newAdminObject.first_name == ""){
+      this.first_name_required_error = true;
+      error_count += 1;
+    }else{
+      this.first_name_required_error = false;
+    }
+
+    if(this.newAdminObject.last_name == ""){
+      this.last_name_required_error = true;
+      error_count += 1;
+    }else{
+      this.last_name_required_error = false;
+    }
+
+    if( !this.validateEmail(this.newAdminObject.email) ){ //validate email
+      this.email_validation_error_flag = true;
+      error_count += 1;
+    }else{
+      this.email_validation_error_flag = false;
+    }
+
+    if(this.newAdminObject.password == ""){
+      error_count += 1;
+      this.registration_password_error_flag = true;
+    }else{
+      this.registration_password_error_flag = false;
+    }
+
+    if(this.newAdminObject.password != this.password_confirm){ //password confirmation match check
+      this.password_confirm_error_flag = true;
+      error_count += 1;
+    }else{
+      this.password_confirm_error_flag = false;
+    }
+
+    if(error_count > 0){
+     return false;
+    }else{
+      return true;
+    }
+  }
+
   postAdminToService(){
-    console.log(this.newAdminObject.admin_id);
-    //this.newAdminObject.admin_id = 0; //will be changed on the backend
-    this._httpService.postAdmin<Admin>(this.newAdminObject).subscribe(
-      result => {
-        console.log(result);
-        let incomingAdmin:any = result; //it's an Admin though.
-        this.logged_admin.admin_id = incomingAdmin.admin_id;
-        this.logged_admin.first_name = incomingAdmin.first_name;
-        this.logged_admin.last_name = incomingAdmin.last_name;
-        this.logged_admin.email = incomingAdmin.email;
-        this.logged_admin.password = incomingAdmin.password;
-        this.logged_admin.token = incomingAdmin.token;
-        this.logEvent.emit(this.logged_admin);
-      });
+    if (this.validateRegistration()){
+      console.log(this.newAdminObject.admin_id);
+      //this.newAdminObject.admin_id = 0; //will be changed on the backend
+      this._httpService.postAdmin<Admin>(this.newAdminObject).subscribe(  //send new admin to backend
+        result => {
+          console.log(result);
+          let incomingAdmin:any = result; //it's an Admin though.
+          this.logged_admin.admin_id = incomingAdmin.admin_id;
+          this.logged_admin.first_name = incomingAdmin.first_name;
+          this.logged_admin.last_name = incomingAdmin.last_name;
+          this.logged_admin.email = incomingAdmin.email;
+          this.logged_admin.password = incomingAdmin.password;
+          this.logged_admin.token = incomingAdmin.token;
+          this.logEvent.emit(this.logged_admin);
+        });
+    }
+  }
+
+  validateLogin(){
+    let error_count:number = 0;
+    if(this.validateEmail(this.entered_email)){
+      this.login_email_validation_error_flag = false;
+    }else{
+      error_count += 1;
+      this.login_email_validation_error_flag = true;
+    }
+
+    if(this.entered_password == ""){
+      error_count += 1;
+      this.login_password_validation_error_flag = true;
+    }else{
+      this.login_password_validation_error_flag = false;
+    }
+
+    if(error_count > 0){
+      return false;
+    } else {
+      return true;
+    }
   }
 
   loginAdmin(){
-    let login_package = {
-      email: this.entered_email,
-      password: this.entered_password
+    if(this.validateLogin()){
+      let login_package = {
+        email: this.entered_email,
+        password: this.entered_password
+      }
+  
+        this._httpService.getAdminByEmail(login_package).subscribe(
+          result => {
+            console.log(result);
+            let incomingAdmin:any = result;
+            if(incomingAdmin.first_name === "<ACCESS DENIED, Password Invalid>"){
+              this.error_display = "Invalid password";
+            }
+    
+            if(incomingAdmin.first_name === "<NOT FOUND, Email invalid>"){
+              this.error_display = "Email not registered";
+            }
+    
+            if(incomingAdmin.email === this.entered_email && incomingAdmin.password == this.entered_password){
+              this.logged_admin = incomingAdmin;
+              this.logEvent.emit(this.logged_admin); //zero is the default non sql ID value
+              this.error_display = "";
+            }else{
+              this.error_display = "Invalid Email or Password"
+            }
+          }
+        );
+      }
     }
 
-    this._httpService.getAdminByEmail(login_package).subscribe(
-      result => {
-        console.log(result);
-        let incomingAdmin:any = result;
-        if(incomingAdmin.first_name === "<ACCESS DENIED, Password Invalid>"){
-          this.error_display = "Invalid password";
-        }
-
-        if(incomingAdmin.first_name === "<NOT FOUND, Email invalid>"){
-          this.error_display = "Email not registered";
-        }
-
-        if(incomingAdmin.email === this.entered_email && incomingAdmin.password == this.entered_password){
-          this.logged_admin = incomingAdmin;
-          this.logEvent.emit(this.logged_admin); //zero is the default non sql ID value
-          this.error_display = "";
-        }
-      }
-    )
-  }
 }
 
 interface Admin {
