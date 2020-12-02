38719360a0f38bc59a9d14f1012784c40e021fcf
seriouschess <rehayemb@gmail.com>
2020-12-02 16:15:39 -0600

    video tutorials now contained in a selection module.

--- ClientApp/src/app/editor/tutorial/tutorial.component.ts
+++ ClientApp/src/app/editor/tutorial/tutorial.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tutorial',
@@ -7,21 +8,59 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TutorialComponent implements OnInit {
 
-  constructor() { }
+  constructor(public sanitizer: DomSanitizer) { }
 
   open_site_select: boolean;
   open_editor: boolean;
   display_sites_done: boolean;
-
+  open_guide_videos:boolean
+  video_list:TutorialVideo[];
 
   ngOnInit() {
     this.open_site_select = false;
     this.open_editor = false;
     this.display_sites_done = false;
+    this.addVideos();
   }
 
   beginTutorial(){
     this.open_site_select = true;
+  }
+
+  openGuideVideos(){
+    this.open_guide_videos = !this.open_guide_videos;
+  }
+
+  addVideos(){
+    this.video_list = [];
+    let video_one:TutorialVideo = {
+      video_title: "General guide video",
+      source_url: this.dodgeTheBus("https://www.youtube.com/embed/TeixVm1TFQY"),
+      selected: false,
+      description: ""
+    }
+
+    this.video_list.push(video_one);
+
+    let video_two:TutorialVideo = {
+      video_title: "How to link multipage sites",
+      source_url: this.dodgeTheBus("https://www.youtube.com/embed/EAAiXQCHs8c"),
+      selected: false,
+      description: ""
+    }
+
+    this.video_list.push(video_two);
+  }
+
+  dodgeTheBus(str:string):SafeResourceUrl{
+    return this.sanitizer.bypassSecurityTrustResourceUrl(str);
+  }
+
+  toggleVideoSelection(inc_video:TutorialVideo){
+    this.video_list.forEach(
+      current_video => current_video.selected = false
+    );
+    inc_video.selected = true;
   }
 
   checkDone($event){
@@ -29,4 +68,11 @@ export class TutorialComponent implements OnInit {
     this.open_editor = true;
     this.open_site_select = false;
   }
+}
+
+interface TutorialVideo{
+  video_title: string,
+  source_url: SafeResourceUrl,
+  selected: boolean,
+  description:string
 }
